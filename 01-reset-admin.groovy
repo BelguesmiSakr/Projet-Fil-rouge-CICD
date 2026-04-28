6ab28fbe6c0bf5f8b41756945ae19a645b97941d
Amani Cherif <amanicherif431@gmail.com>
2026-04-28 16:46:28 +0200

    update

---
+++ 01-reset-admin.groovy
@@ -0,0 +1,9 @@
+﻿import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin","Admin123!")
+instance.setSecurityRealm(hudsonRealm)
+instance.setAuthorizationStrategy(new FullControlOnceLoggedInAuthorizationStrategy())
+instance.save()
